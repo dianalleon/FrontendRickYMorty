869067a3804de6d40a1527f854eabae29bd9ac57
dianalleon <diana.leon@gopenux.com>
2024-04-05 13:12:45 -0500

    cambios componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import { ListCharacterComponent } from './platform/list-character/list-character.component';
+import { ListComponent } from './platform/components/list/list.component';
+import { CharacterComponent } from './platform/components/character/character.component';
+import { DetailsCharacterComponent } from './platform/components/details-character/details-character.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListCharacterComponent
+    ListCharacterComponent,
+    ListComponent,
+    CharacterComponent,
+    DetailsCharacterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/platform/components/details-character/details-character.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-character',
+  templateUrl: './details-character.component.html',
+  styleUrls: ['./details-character.component.sass']
+})
+export class DetailsCharacterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
